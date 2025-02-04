68127894761b0800b1ef95d9b0457b0d8414c241
Charles-Antoine Fournier <charlesantoinef854@gmail.com>
2025-02-03 20:04:44 -0500

    first variables

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const quote = document.getElementById('quote');
+console.log(quote);
+const author = document.getElementById('author');
+const button = document.getElementById('button');
+
+const quotes = [];
+const quote1 = {
+    citation: "Im stupid, Im ugly, Im dumb, I smell. Did I mention Im stupid?",
+    auth: 'Eminem'
+};
+
+console.log('citation', quote1.citation);
